747d7c64926aa422726cd872258db3bac7f3929e
Zichen Wang <wangzc921@gmail.com>
2017-04-03 12:11:58 -0400

    Improved drug_class mappings

--- scripts/update_metadf.py
+++ scripts/update_metadf.py
@@ -30,10 +30,25 @@ graph = json.load(open(ljp_json_file, 'rb'))
 
 drug_classes = [node['DrugClass'] for node in graph['nodes']]
 pert_ids = [node['id'].split(':')[1] for node in graph['nodes']]
+drug_names = [node['DrugName'] for node in graph['nodes']]
 
 d_pertid_drugclass = dict(zip(pert_ids, drug_classes))
+d_name_drugclass = dict(zip(drug_names, drug_classes))
 
-meta_df['drug_class'] = meta_df['pert_id'].map(lambda x: d_pertid_drugclass.get(x, 'unannotated'))
+# meta_df['drug_class'] = meta_df['pert_id'].map(lambda x: d_pertid_drugclass.get(x, 'unannotated'))
+drug_classes = []
+for i, row in meta_df.iterrows():
+	pert_id = row['pert_id']
+	name = row['perturbation']
+	if pert_id in d_pertid_drugclass:
+		drug_class = d_pertid_drugclass[pert_id]
+	elif name in d_name_drugclass:
+		drug_class = d_name_drugclass[name]
+	else:
+		drug_class = 'unannotated'
+	drug_classes.append(drug_class)
+
+meta_df['drug_class'] = drug_classes
 
 print meta_df.head()
 print meta_df['drug_class'].value_counts()
@@ -41,6 +56,7 @@ print meta_df.count()
 
 pert_meta_df = meta_df[['pert_id', 'perturbation', 'drug_class']].drop_duplicates().set_index('pert_id')
 # print pert_meta_df.head()
+print pert_meta_df['drug_class'].value_counts()
 
 # write to files
 meta_df.to_csv('../data/metadata-full-anno.tsv', sep='\t')
