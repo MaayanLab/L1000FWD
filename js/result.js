5300936ff5c49ae627c87d4a1e5628b80e1b2f0d
Zichen Wang <wangzc921@gmail.com>
2017-06-09 13:48:17 -0400

    Moved DOMs on the result page to the correct locations

--- js/result.js
+++ js/result.js
@@ -5,7 +5,14 @@ var sd = new ScatterData({
 	url: 'result/graph/' + result_id
 })
 
+var container = document.getElementById("body")
+var width = container.clientWidth;
+var height = container.clientHeight;
+
 var sdv = new Scatter3dView({
+	container: container,
+	WIDTH: width,
+	HEIGHT: height,	
 	model: sd,
 	textures: textures,
 	// pointSize: 0.1, 
@@ -16,15 +23,15 @@ var sdv = new Scatter3dView({
 	labelKey: ['sig_id', 'perturbation'],
 })
 
-var legend = new Legend({scatterPlot: sdv, h: window.innerHeight})
+var legend = new Legend({scatterPlot: sdv, h: window.innerHeight, container: container})
 
-var controler = new Controler({scatterPlot: sdv, h: window.innerHeight, w: 200})
+var controler = new Controler({scatterPlot: sdv, h: window.innerHeight, w: 200, container: container})
 
 var search = new SearchSelectize({scatterPlot: sdv, container: "#controls"})
 
 var sigSimSearch = new SigSimSearchForm({scatterPlot: sdv, container: "#controls", result_id: result_id})
 
-var resultModalBtn = new ResultModalBtn({scatterPlot: sdv, container: document.body, result_id: result_id})
+var resultModalBtn = new ResultModalBtn({scatterPlot: sdv, container: document.body, result_id: result_id, container: container})
 
 var resultModal = new ResultModal({scatterPlot: sdv});
 
